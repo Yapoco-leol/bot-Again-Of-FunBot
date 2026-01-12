13b4f18233231592a4bf8f2ce7f85688becb14f1
Yapoco <hrenandezarmanto735@gmail.com>
2026-01-12 08:42:31 -0600

    Mi primer bot de discord

--- carpeta-bot/index.js
+++ carpeta-bot/index.js
@@ -1,1 +1,10 @@
+// 1. Cargar la configuración del archivo .env
+require('dotenv').config();
 
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// 2. Acceder al token usando process.env
+const token = process.env.DISCORD_TOKEN;
+
+client.login(token);
